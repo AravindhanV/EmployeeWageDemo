cb4310f11f332f7584f181a61a672ef9f866da37
Aravindhan V <aravindhan.v@perfios.com>
2021-08-20 12:23:35 +0530

    ADDED: Calculator now supports part time

--- src/com/bridgelabz/EmployeeWageCalc/EmployeeWageCalc.java
+++ src/com/bridgelabz/EmployeeWageCalc/EmployeeWageCalc.java
@@ -1,16 +1,22 @@
 package com.bridgelabz.EmployeeWageCalc;
 
 public class EmployeeWageCalc {
-	public static final int employeePresent = 1;
-	public static final int fullTime = 8;
+	public static final int fullTimeHrs = 8;
 	public static final int wagePerHour = 20;
+	public static final int fullTime = 2;
+	public static final int partTime = 1;
+	public static final int absent = 0;
 	
 	public static void main(String[] args) {
 		
-		double empCheck = Math.floor(Math.random()*10)%2;
-		int dailyWage = 0,hours=0;
-		if(empCheck == employeePresent) {
-			hours = fullTime;
+		double empCheck = Math.floor(Math.random()*10)%3;
+		int hours=0;
+		if(empCheck == fullTime) {
+			hours = fullTimeHrs;
+		} else if(empCheck == partTime) {
+			hours = fullTimeHrs/2;
+		} else {
+			hours = 0;
 		}
 		System.out.println("Daily Wage = "+(hours*wagePerHour));		
 	}
