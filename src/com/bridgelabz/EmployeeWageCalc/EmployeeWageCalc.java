c6f3db6e48c2be414fb6fff4cbf51ce65fcd92a0
Aravindhan V <aravindhan.v@perfios.com>
2021-08-20 12:06:59 +0530

    ADDED: Employee Attendance

--- src/com/bridgelabz/EmployeeWageCalc/EmployeeWageCalc.java
+++ src/com/bridgelabz/EmployeeWageCalc/EmployeeWageCalc.java
@@ -2,6 +2,14 @@ package com.bridgelabz.EmployeeWageCalc;
 
 public class EmployeeWageCalc {
 	public static void main(String[] args) {
-		System.out.println("Employee Wage Calculator");
+		int employeePresent = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		
+		if(empCheck == employeePresent) {
+			System.out.println("Employee Present");
+		}
+		else {
+			System.out.println("Employee Absent");
+		}
 	}
 }
